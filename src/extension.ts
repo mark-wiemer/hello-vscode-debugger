6b1ff893e090b76daff8551e87633866a3699408
Mark Wiemer <markwiemer@outlook.com>
2023-08-14 18:06:46 -0700

    Fix typo

--- src/extension.ts
+++ src/extension.ts
@@ -71,7 +71,7 @@ class DebugAdapterExecutableFactory
     implements vscode.DebugAdapterDescriptorFactory
 {
     // The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
-    // Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
+    // Since the code implements the default behavior, it is absolutely not necessary and we show it here only for educational purpose.
 
     createDebugAdapterDescriptor(
         _session: vscode.DebugSession,
